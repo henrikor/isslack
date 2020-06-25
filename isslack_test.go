6ced77b4303d9bde3ed56aefa1c57ae9037dee31
Henrik Ormåsen <henrik.ormasen@cgi.com>
2020-06-25 16:32:51 +0200

    slack url

--- isslack_test.go
+++ isslack_test.go
@@ -2,9 +2,11 @@ package isslack
 
 import "testing"
 
+// TODO:
+// must take SLACK URL as argument
 func TestIsslack(t *testing.T) {
 	want := "200 OK"
-	if got := Isslack("Test", "https://hooks.slack.com/services/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"); got != want {
+	if got := Isslack("Test", "SLACK URL"); got != want {
 		t.Errorf("Isslack() = %q, want %q", got, want)
 	}
 }
