8bbd769fe372e81177e16014a81a8c2d2ecd62e1
Henrik Ormåsen <henrik.ormasen@cgi.com>
2020-06-25 15:43:23 +0200

    removed webhook url

--- isslack.go
+++ isslack.go
@@ -12,7 +12,6 @@ import (
 // isslack("ismetering", "install")
 // func postSlack(msg string, surl string) string {
 func Isslack(msg string, surl string) string {
-	// surl := "https://hooks.slack.com/services/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"
 	message := map[string]interface{}{
 		"text": msg,
 	}
